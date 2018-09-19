256915a88fc2ee2bf3081cd8392a510a2ae3b3a8
Levin Zhu <spiritlevin@qq.com>
2018-09-19 10:26:40 +0800

    更换版本

--- scratch/gui.js
+++ scratch/gui.js
@@ -1,36 +1,11 @@
 var GUI =
-(window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([["gui"],{
+(window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([[1],{
 
-/***/ "./node_modules/css-loader/index.js?!./node_modules/postcss-loader/src/index.js?!./src/playground/index.css":
-/*!*****************************************************************************************************************!*\
-  !*** ./node_modules/css-loader??ref--5-1!./node_modules/postcss-loader/src??postcss!./src/playground/index.css ***!
-  \*****************************************************************************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-exports = module.exports = __webpack_require__(/*! ../../node_modules/css-loader/lib/css-base.js */ "./node_modules/css-loader/lib/css-base.js")(false);
-// imports
-
-
-// module
-exports.push([module.i, "html,\nbody,\n.index_app_3Qs6X {\n    /* probably unecessary, transitional until layout is refactored */\n    width: 100%; \n    height: 100%;\n    margin: 0;\n\n    /* Setting min height/width makes the UI scroll below those sizes */\n    min-width: 1024px;\n    min-height: 640px; /* Min height to fit sprite/backdrop button */\n}\n\n/* @todo: move globally? Safe / side FX, for blocks particularly? */\n\n* { -webkit-box-sizing: border-box; box-sizing: border-box; }\n", ""]);
-
-// exports
-exports.locals = {
-	"app": "index_app_3Qs6X"
-};
-
-/***/ }),
-
-/***/ "./src/playground/index.css":
-/*!**********************************!*\
-  !*** ./src/playground/index.css ***!
-  \**********************************/
-/*! no static exports found */
+/***/ 1097:
 /***/ (function(module, exports, __webpack_require__) {
 
 
-var content = __webpack_require__(/*! !../../node_modules/css-loader??ref--5-1!../../node_modules/postcss-loader/src??postcss!./index.css */ "./node_modules/css-loader/index.js?!./node_modules/postcss-loader/src/index.js?!./src/playground/index.css");
+var content = __webpack_require__(1098);
 
 if(typeof content === 'string') content = [[module.i, content, '']];
 
@@ -44,7 +19,7 @@ var options = {"hmr":true}
 options.transform = transform
 options.insertInto = undefined;
 
-var update = __webpack_require__(/*! ../../node_modules/style-loader/lib/addStyles.js */ "./node_modules/style-loader/lib/addStyles.js")(content, options);
+var update = __webpack_require__(5)(content, options);
 
 if(content.locals) module.exports = content.locals;
 
@@ -52,79 +27,24 @@ if(false) {}
 
 /***/ }),
 
-/***/ "./src/playground/index.jsx":
-/*!**********************************!*\
-  !*** ./src/playground/index.jsx ***!
-  \**********************************/
-/*! no static exports found */
+/***/ 1098:
 /***/ (function(module, exports, __webpack_require__) {
 
-"use strict";
+exports = module.exports = __webpack_require__(4)(false);
+// imports
 
 
-__webpack_require__(/*! es6-object-assign/auto */ "./node_modules/es6-object-assign/auto.js");
+// module
+exports.push([module.i, "html,\nbody,\n.index_app_3Qs6X {\n    /* probably unecessary, transitional until layout is refactored */\n    width: 100%; \n    height: 100%;\n    margin: 0;\n\n    /* Setting min height/width makes the UI scroll below those sizes */\n    min-width: 1024px;\n    min-height: 640px; /* Min height to fit sprite/backdrop button */\n}\n\n/* @todo: move globally? Safe / side FX, for blocks particularly? */\n\n* { -webkit-box-sizing: border-box; box-sizing: border-box; }\n", ""]);
 
-__webpack_require__(/*! core-js/fn/array/includes */ "./node_modules/core-js/fn/array/includes.js");
-
-__webpack_require__(/*! intl */ "./node_modules/intl/index.js");
-
-var _react = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-
-var _react2 = _interopRequireDefault(_react);
-
-var _reactDom = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
-
-var _reactDom2 = _interopRequireDefault(_reactDom);
-
-var _analytics = __webpack_require__(/*! ../lib/analytics */ "./src/lib/analytics.js");
-
-var _analytics2 = _interopRequireDefault(_analytics);
-
-var _appStateHoc = __webpack_require__(/*! ../lib/app-state-hoc.jsx */ "./src/lib/app-state-hoc.jsx");
-
-var _appStateHoc2 = _interopRequireDefault(_appStateHoc);
-
-var _browserModal = __webpack_require__(/*! ../components/browser-modal/browser-modal.jsx */ "./src/components/browser-modal/browser-modal.jsx");
-
-var _browserModal2 = _interopRequireDefault(_browserModal);
-
-var _supportedBrowser = __webpack_require__(/*! ../lib/supported-browser */ "./src/lib/supported-browser.js");
-
-var _supportedBrowser2 = _interopRequireDefault(_supportedBrowser);
-
-var _index = __webpack_require__(/*! ./index.css */ "./src/playground/index.css");
-
-var _index2 = _interopRequireDefault(_index);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-// Register "base" page view
-// Polyfills
-_analytics2.default.pageview('/'); // For Safari 9
-
-var appTarget = document.createElement('div');
-appTarget.className = _index2.default.app;
-document.body.appendChild(appTarget);
-
-if ((0, _supportedBrowser2.default)()) {
-    // require needed here to avoid importing unsupported browser-crashing code
-    // at the top level
-    __webpack_require__(/*! ./render-gui.jsx */ "./src/playground/render-gui.jsx").default(appTarget);
-} else {
-    _browserModal2.default.setAppElement(appTarget);
-    var WrappedBrowserModalComponent = (0, _appStateHoc2.default)(_browserModal2.default, true /* localesOnly */);
-    var handleBack = function handleBack() {};
-    // eslint-disable-next-line react/jsx-no-bind
-    _reactDom2.default.render(_react2.default.createElement(WrappedBrowserModalComponent, { onBack: handleBack }), appTarget);
-}
+// exports
+exports.locals = {
+	"app": "index_app_3Qs6X"
+};
 
 /***/ }),
 
-/***/ "./src/playground/render-gui.jsx":
-/*!***************************************!*\
-  !*** ./src/playground/render-gui.jsx ***!
-  \***************************************/
-/*! no static exports found */
+/***/ 1099:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -136,27 +56,27 @@ Object.defineProperty(exports, "__esModule", {
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-var _react = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+var _react = __webpack_require__(0);
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactDom = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
+var _reactDom = __webpack_require__(33);
 
 var _reactDom2 = _interopRequireDefault(_reactDom);
 
-var _appStateHoc = __webpack_require__(/*! ../lib/app-state-hoc.jsx */ "./src/lib/app-state-hoc.jsx");
+var _appStateHoc = __webpack_require__(113);
 
 var _appStateHoc2 = _interopRequireDefault(_appStateHoc);
 
-var _gui = __webpack_require__(/*! ../containers/gui.jsx */ "./src/containers/gui.jsx");
+var _gui = __webpack_require__(161);
 
 var _gui2 = _interopRequireDefault(_gui);
 
-var _hashParserHoc = __webpack_require__(/*! ../lib/hash-parser-hoc.jsx */ "./src/lib/hash-parser-hoc.jsx");
+var _hashParserHoc = __webpack_require__(181);
 
 var _hashParserHoc2 = _interopRequireDefault(_hashParserHoc);
 
-var _titledHoc = __webpack_require__(/*! ../lib/titled-hoc.jsx */ "./src/lib/titled-hoc.jsx");
+var _titledHoc = __webpack_require__(453);
 
 var _titledHoc2 = _interopRequireDefault(_titledHoc);
 
@@ -179,23 +99,88 @@ exports.default = function (appTarget) {
         visible: true,
         host: backpackHost
     };
-    if (false) {}
+    if ("production" === 'production' && (typeof window === 'undefined' ? 'undefined' : _typeof(window)) === 'object') {
+        // Warn before navigating away
+        window.onbeforeunload = function () {
+            return true;
+        };
+    }
 
     _reactDom2.default.render(_react2.default.createElement(WrappedGui, { backpackOptions: backpackOptions }), appTarget);
 };
 
 /***/ }),
 
-/***/ 1:
-/*!*******************************************!*\
-  !*** ./locale-data/complete.js (ignored) ***!
-  \*******************************************/
-/*! no static exports found */
+/***/ 471:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(472);
+
+__webpack_require__(474);
+
+__webpack_require__(498);
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = __webpack_require__(33);
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+var _analytics = __webpack_require__(28);
+
+var _analytics2 = _interopRequireDefault(_analytics);
+
+var _appStateHoc = __webpack_require__(113);
+
+var _appStateHoc2 = _interopRequireDefault(_appStateHoc);
+
+var _browserModal = __webpack_require__(379);
+
+var _browserModal2 = _interopRequireDefault(_browserModal);
+
+var _supportedBrowser = __webpack_require__(383);
+
+var _supportedBrowser2 = _interopRequireDefault(_supportedBrowser);
+
+var _index = __webpack_require__(1097);
+
+var _index2 = _interopRequireDefault(_index);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// Register "base" page view
+// Polyfills
+_analytics2.default.pageview('/'); // For Safari 9
+
+var appTarget = document.createElement('div');
+appTarget.className = _index2.default.app;
+document.body.appendChild(appTarget);
+
+if ((0, _supportedBrowser2.default)()) {
+    // require needed here to avoid importing unsupported browser-crashing code
+    // at the top level
+    __webpack_require__(1099).default(appTarget);
+} else {
+    _browserModal2.default.setAppElement(appTarget);
+    var WrappedBrowserModalComponent = (0, _appStateHoc2.default)(_browserModal2.default, true /* localesOnly */);
+    var handleBack = function handleBack() {};
+    // eslint-disable-next-line react/jsx-no-bind
+    _reactDom2.default.render(_react2.default.createElement(WrappedBrowserModalComponent, { onBack: handleBack }), appTarget);
+}
+
+/***/ }),
+
+/***/ 500:
 /***/ (function(module, exports) {
 
 /* (ignored) */
 
 /***/ })
 
-},[["./src/playground/index.jsx","lib.min"]]]);
+},[[471,0]]]);
 //# sourceMappingURL=gui.js.map
